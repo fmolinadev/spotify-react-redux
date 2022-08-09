ca9fc3d6cd18fa302ef7e35dcd77bb59b4bff6a7
Francisco Molina <fdm.molina@gmail.com>
2022-08-09 20:23:38 -0300

    Create the player component

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,6 +1,82 @@
 import * as Styles from "./PlayerStyles";
+import PlaceholderCover from "../../assets/placeholder_cover.png";
+// import HeartSimple from "../../assets/heart-free-icon-font.png";
+import HeartLike from "../../assets/heart-like-free-icon-font.png";
+import ExpandCard from "../../assets/card-free-icon-font.png";
+import RandomOff from "../../assets/shuffle-off-free-icon-font.png";
+// import RandomOn from "../../assets/shuffle-free-icon-font.png";
+import PrevTrack from "../../assets/back-free-icon-font.png";
+import NextTrack from "../../assets/next-free-icon-font.png";
+import Play from "../../assets/play-free-icon-font.png";
+// import Pause from "../../assets/pause-free-icon-font.png";
+import RepeatOff from "../../assets/repeat-off-free-icon-font.png";
+// import Repeat from "../../assets/repeat-free-icon-font.png";
+import Microphone from "../../assets/microfono.png";
+import List from "../../assets/cola.png";
+import Connect from "../../assets/conecta-y-reproduce.png";
+import VolumeMax from "../../assets/volumen-alto.png";
+// import VolumeMed from "../../assets/volumen-medio.png"
+// import VolumeMin from "../../assets/volumen-bajo.png"
+// import Mute from "../../assets/volumen-mute.png"
 const Player = () => {
-  return <Styles.Container>Player</Styles.Container>;
+  return (
+    <Styles.Container>
+      <Styles.DataContainer>
+        <Styles.Placeholder src={PlaceholderCover} alt="cover track" />
+        <Styles.DataTrack>
+          <h4>Titulo</h4>
+          <p>Artista</p>
+        </Styles.DataTrack>
+        <Styles.DataState>
+          <Styles.LikeNull src={HeartLike} alt="favourite" />
+          <Styles.Expand src={ExpandCard} alt="expand" />
+        </Styles.DataState>
+      </Styles.DataContainer>
+      <Styles.ControlContainer>
+        <Styles.AllButtoms>
+          <li>
+            <Styles.IconController src={RandomOff} alt="repeat" />
+          </li>
+          <li>
+            <Styles.IconController src={PrevTrack} alt="prev" />
+          </li>
+          <li>
+            <Styles.PlayController src={Play} alt="play" />
+          </li>
+          <li>
+            <Styles.IconController src={NextTrack} alt="next" />
+          </li>
+          <li>
+            <Styles.IconController src={RepeatOff} alt="repeat-control" />
+          </li>
+        </Styles.AllButtoms>
+        <Styles.TimeView>
+          <p>00:00</p>
+          <hr />
+          <p>02:45</p>
+        </Styles.TimeView>
+      </Styles.ControlContainer>
+      <Styles.SettingContainer>
+        <Styles.AllSettings>
+          <li>
+            <Styles.IconSetting src={Microphone} alt="karaoke" />
+          </li>
+          <li>
+            <Styles.IconSetting src={List} alt="stack" />
+          </li>
+          <li>
+            <Styles.IconSetting src={Connect} alt="connect" />
+          </li>
+          <li>
+            <Styles.VolumeControl>
+              <Styles.IconSetting src={VolumeMax} alt="volume" />
+              <input type="range" min="0" max="100" step="1" />
+            </Styles.VolumeControl>
+          </li>
+        </Styles.AllSettings>
+      </Styles.SettingContainer>
+    </Styles.Container>
+  );
 };
 
 export default Player;
--- src/components/Player/PlayerStyles.js
+++ src/components/Player/PlayerStyles.js
@@ -2,9 +2,182 @@ import styled from "styled-components";
 
 export const Container = styled.section`
   position: fixed;
+  display: flex;
+  justify-content: space-between;
+  margin: auto;
   width: 100vw;
   height: 90px;
   background-color: #181818;
   color: white;
   bottom: 0;
+  align-items: center;
+`;
+
+export const DataContainer = styled.div`
+  display: flex;
+  flex: 0.3;
+  max-width: 465px;
+`;
+
+export const Placeholder = styled.img`
+  width: 55px;
+  padding: 15px;
+  object-fit: contain;
+`;
+
+export const DataTrack = styled.div`
+  padding: 25px 0px;
+  align-items: center;
+  width: 100px;
+  min-width: 90px;
+  & > h4 {
+    color: white;
+    font-size: 14px;
+    font-weight: 500;
+    font-family: "Montserrat", sans-serif;
+    padding-bottom: 2px;
+  }
+  & > p {
+    color: white;
+    font-size: 11px;
+    font-family: "Montserrat", sans-serif;
+    font-weight: light;
+    padding-top: 2px;
+  }
+`;
+
+export const DataState = styled.div`
+  padding: 37px 8px 2px 19px;
+  height: 25px;
+  min-width: 90px;
+  align-items: center;
+`;
+
+export const LikeNull = styled.img`
+  width: 15px;
+`;
+
+export const Expand = styled.img`
+  width: 16px;
+  padding-left: 13px;
+`;
+
+export const ControlContainer = styled.div`
+  flex: 0.4;
+  flex-direction: row;
+  align-items: center;
+  width: 100%;
+  justify-content: space-between;
+  margin: 2px 50px;
+`;
+
+export const AllButtoms = styled.ul`
+  display: flex;
+  margin: auto;
+  color: white;
+  padding-top: 8px;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  & > li {
+    list-style-type: none;
+    opacity: 0.8;
+  }
+  & > li:hover {
+    opacity: 1;
+  }
+`;
+
+export const IconController = styled.img`
+  width: 15px;
+  padding: 16px 18px;
+  margin: auto;
+`;
+
+export const PlayController = styled.img`
+  width: 22px;
+  height: 20px;
+  padding: 8px 8px;
+  margin-top: 5px;
+  background-color: white;
+  border-radius: 50%;
+  transition: transform 0.1s;
+  &:active {
+    transform: scale(0.9);
+  }
+`;
+
+export const TimeView = styled.div`
+  display: flex;
+  padding-top: 2px;
+  width: 100%;
+
+  & > p {
+    font-size: 10px;
+    font-family: "Montserrat", sans-serif;
+    font-weight: regular;
+    padding: 0px 9px;
+  }
+  & > hr {
+    height: 3px;
+    width: 90%;
+    border-radius: 6px;
+    padding-top: 1px;
+    margin-top: 4px;
+  }
+`;
+
+export const SettingContainer = styled.div`
+  display: flex;
+  flex: 0.3;
+  align-items: center;
+  max-width: 465px;
+  right: 0;
+`;
+
+export const AllSettings = styled.ul`
+  display: flex;
+  margin: auto;
+  color: white;
+  padding-top: 8px;
+  width: 100%;
+  list-style-type: none;
+  justify-content: center;
+  align-items: center;
+  & > li {
+    list-style-type: none;
+    opacity: 0.8;
+  }
+  & > li:hover {
+    opacity: 1;
+  }
+`;
+
+export const IconSetting = styled.img`
+  width: 18px;
+  padding: 12px 10px;
+  margin: auto;
+`;
+
+export const VolumeControl = styled.div`
+  display: flex;
+  width: 145px;
+  & input {
+    height: 8px;
+    width: 80%;
+    cursor: pointer;
+    overflow: hidden;
+    display: block;
+    appearance: none;
+    border-radius: 6px;
+    padding-top: 1px;
+    margin-top: 17px;
+    background-color: #1ed760;
+  }
+  &:focus {
+    outline: none;
+  }
+  &:-webkit-slider-runnable-track {
+    border-radius: 20px;
+  }
 `;
