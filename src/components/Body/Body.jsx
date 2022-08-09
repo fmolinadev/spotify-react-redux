00672c165318304ec616f756a3d571d53ffb0c08
Francisco Molina <fdm.molina@gmail.com>
2022-08-08 23:54:02 -0300

    Update body

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,7 +1,16 @@
 import * as Styles from "./BodyStyles";
+import Sidebar from "../Sidebar/Sidebar";
+import Player from "../Player/Player";
+import Main from "../Main/Main";
 
 const Body = () => {
-  return <Styles.Container>Body</Styles.Container>;
+  return (
+    <Styles.Container>
+      <Sidebar />
+      <Main />
+      <Player />
+    </Styles.Container>
+  );
 };
 
 export default Body;
--- src/components/Body/BodyStyles.js
+++ src/components/Body/BodyStyles.js
@@ -1,3 +1,5 @@
 import styled from "styled-components";
 
-export const Container = styled.body``;
+export const Container = styled.body`
+  display: flex;
+`;
