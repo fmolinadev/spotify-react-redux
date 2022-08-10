af9642697907ec06c9d9a7685e8bc65c8c97bcd9
Francisco Molina <fdm.molina@gmail.com>
2022-08-09 23:20:49 -0300

    Modules login function

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,22 +1,7 @@
 import * as Style from "./LoginStyles";
 import LogoBlack from "../../assets/Spotify_Logo_Black.png";
+import { toLogin } from "../../utils/login";
 const Login = () => {
-  const endpointLogin = `https://accounts.spotify.com/authorize`;
-  const myClientID = "6e68b40ac15344c2b31b7b1478260ac4";
-  const redirectURI = "http://localhost:3000/";
-  const scopes = [
-    "user-top-read",
-    "user-read-recently-played",
-    "user-read-currently-playing",
-    "user-read-playback-state",
-    "user-library-read",
-    "user-modify-playback-state",
-  ];
-
-  const toLogin = `${endpointLogin}?client_id=${myClientID}&response_type=token&redirect_uri=${redirectURI}&scope=${scopes.join(
-    "%20"
-  )}&show_dialog=true`;
-
   return (
     <Style.Container>
       <Style.LogoContainer>
