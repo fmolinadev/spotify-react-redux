c6afcc688efa9aa3934dd13524182f76abe3061d
Francisco Molina <fdm.molina@gmail.com>
2022-08-09 23:21:52 -0300

    Update App with Login access token

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { useState } from "react";
+import { useEffect } from "react";
 import Body from "./components/Body/Body";
-// import Login from "./components/Login/Login.jsx";
+import Login from "./components/Login/Login.jsx";
+import { loginToken } from "./utils/login";
+
 const App = () => {
-  return (
-    <>
-      {/* <Login /> */}
-      <Body />
-    </>
-  );
+  const [token, setToken] = useState();
+
+  useEffect(() => {
+    const tokenFromSpotify = loginToken();
+
+    const getToken = tokenFromSpotify.access_token;
+    console.log("GET TOKEN-->", getToken);
+    setToken(getToken);
+  }, []);
+
+  return <>{token ? <Body /> : <Login />}</>;
 };
 
 export default App;
