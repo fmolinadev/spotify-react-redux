4ec9d6406de3296eb94781053f2a30a3cb962aeb
Francisco Molina <fdm.molina@gmail.com>
2022-08-10 23:39:29 -0300

    Update Store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,18 @@
 import { applyMiddleware } from "redux";
 import { configureStore } from "@reduxjs/toolkit";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { userReducer } from "./slice.user";
+import { userToken } from "./slice.token";
 import thunk from "redux-thunk";
-import rootReducer from ".reducer";
+import { userPlaylist } from "./slice.playlist";
 
-export const store = configureStore(
-  rootReducer,
+export default configureStore(
+  {
+    reducer: {
+      user: userReducer.reducer,
+      token: userToken.reducer,
+      playlist: userPlaylist.reducer,
+    },
+  },
   composeWithDevTools(applyMiddleware(thunk))
 );
