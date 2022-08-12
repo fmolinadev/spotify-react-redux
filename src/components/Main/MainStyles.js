ecc65fbbb4df0100e8688a0f6e8fdc56cc88deb0
Francisco Molina <fdm.molina@gmail.com>
2022-08-12 20:33:05 -0300

    Fox main styles

--- src/components/Main/MainStyles.js
+++ src/components/Main/MainStyles.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   width: 100%;
-  background-image: linear-gradient(126.3deg, #202020 32.2%, black 109.2%);
+  background-image: linear-gradient(126.3deg, #101010 32.2%, #24201f 109.2%);
   height: 100vh;
   overflow-y: auto;
   scroll-behavior: smooth;
